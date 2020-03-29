f69b50a2770181d06652c2fb98a08d29ec4ccff9
Yifei Ren <ryf123@gmail.com>
2020-03-28 17:00:32 -0700

    Refactor into class
    Add test cases

--- parse_ca_data.py
+++ parse_ca_data.py
@@ -1,58 +1,34 @@
 import urllib.request
 import re
-from functools import cmp_to_key
+from util import my_print
 
-website_url = 'https://www.cdph.ca.gov'
-main_page_url = website_url + '/Programs/OPA/Pages/New-Release-2020.aspx#'
+class Scrapper:
+	def get_webpage_content(self, url):
+		# return str html
+		try:
+			req = urllib.request.Request(url)
+			response = urllib.request.urlopen(req)
+			webpage_str = response.read()
+			return str(webpage_str)
+		except Exception as e:
+			my_print("error reading:" + url)
+			return None
 
-debug = False
-def my_print(s):
-	if debug:
-		print(s)
+	def get_latest_date(self, s1, s2):
+		d1 = re.findall("/Programs/OPA/Pages/NR20-(\d+).aspx", s1)[0]
+		d2 = re.findall("/Programs/OPA/Pages/NR20-(\d+).aspx", s2)[0]
+		return int(d1) - int(d2)
 
-def get_webpage_content(url):
-	# return str html
-	try:
-		req = urllib.request.Request(url)
-		response = urllib.request.urlopen(req)
-		webpage_str = response.read()
-		return str(webpage_str)
-	except Exception as e:
-		my_print("error reading:" + url)
-		return None
-
-def get_latest_date(s1, s2):
-	d1 = re.findall("/Programs/OPA/Pages/NR20-(\d+).aspx", s1)[0]
-	d2 = re.findall("/Programs/OPA/Pages/NR20-(\d+).aspx", s2)[0]
-	return int(d1) - int(d2)
-
-def get_lastest_page_info(latest_webpage_str):
-	my_print("get page info")
-	date = re.findall(r"Date:\s+(.*),\s+2020<br/>", latest_webpage_str)
-	positive_cases = re.findall(r"([,\d]+)[\\\d\w\s]+Positive cases", latest_webpage_str)
-	death = re.findall(r"<p>([,\d]+) \\xe2\\x80\\x93 Deaths", latest_webpage_str)
-	pending = re.findall(r" At least ([\+,\d]+) results have been received and another ([\+,\d]+) are pending.", latest_webpage_str)
-	return {
-		'date': date[0] if len(date) > 0 else None,
-		'positive_cases': positive_cases[0] if len(positive_cases) > 0 else None,
-		'death': death[0] if len(death) > 0 else None,
-		'received': pending[0][0] if len(pending) else None,
-		'pending': pending[0][1] if len(pending) else None
-	}
-
-# find all occurences of /Programs/OPA/Pages/NR.*.aspx
-all_urls = re.findall(r"(/Programs/OPA/Pages/NR20-\d+.aspx)", get_webpage_content(main_page_url))
-all_urls = sorted(all_urls, key=cmp_to_key(get_latest_date))
-
-# get latest page content
-latest = all_urls[-5]
-latest_webpage_str = get_webpage_content(website_url + latest)
-
-dedup_url = {}
-for url in all_urls[-10:]:
-	if url not in dedup_url:
-		dedup_url[url] = True
-		my_print(url)
-		content = get_webpage_content(website_url + url)
-		if content:
-			print(get_lastest_page_info(content))
+	def get_lastest_page_info(self, latest_webpage_str):
+		my_print("get page info")
+		date = re.findall(r"Date:\s+(.*),\s+2020<br/>", latest_webpage_str)
+		positive_cases = re.findall(r"([,\d]+)[\\\d\w\s]+Positive cases", latest_webpage_str)
+		death = re.findall(r"([,\d]+)[\\\d\w\s]+Deaths", latest_webpage_str)
+		pending = re.findall(r" least ([\+,\d]+) results have been received and another ([\+,\d]+) are pending", latest_webpage_str)
+		return {
+			'date': date[0] if len(date) > 0 else None,
+			'positive_cases': positive_cases[0] if len(positive_cases) > 0 else None,
+			'death': death[0] if len(death) > 0 else None,
+			'results received': pending[0][0] if len(pending) else None,
+			'results pending': pending[0][1] if len(pending) else None
+		}
